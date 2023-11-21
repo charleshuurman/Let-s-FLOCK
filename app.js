05cb7e5c035d566606e60458f3b308777e7b611d
gildardo1098 <gildardo1098@hotmail.com>
2023-11-20 20:44:13 -0600

    changes

--- app.js
+++ app.js
@@ -1,50 +1,61 @@
-require('dotenv').config();
-const express = require('express');
-const { engine } = require('express-handlebars');
-const session = require('express-session');
-const sequelize = require('./config/database');
-const axios = require('axios'); // Import axios for making API requests
+require("dotenv").config();
+const express = require("express");
+const { engine } = require("express-handlebars");
+const session = require("express-session");
+const sequelize = require("./config/database");
+const axios = require("axios"); // Import axios for making API requests
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 // Set up Handlebars as the view engine
-app.engine('handlebars', engine({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.engine("handlebars", engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Body Parser Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Session configuration
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: process.env.NODE_ENV === 'production' }
-}));
+    cookie: { secure: process.env.NODE_ENV === "production" },
+  })
+);
 
 // Import routes
-const headlineSelectionRoutes = require('./routes/headlineSelectionRoutes');
-const newsRoutes = require('./routes/newsRoutes');
-const userChatRoutes = require('./routes/userChatRoutes');
-const userRoutes = require('./routes/userRoutes');
+const headlineSelectionRoutes = require("./routes/headlineSelectionRoutes");
+const newsRoutes = require("./routes/newsRoutes");
+const userChatRoutes = require("./routes/userChatRoutes");
+const userRoutes = require("./routes/userRoutes");
 
 // Using routes
-app.use('/headline-selections', headlineSelectionRoutes);
-app.use('/news', newsRoutes);
-app.use('/chats', userChatRoutes);
-app.use('/users', userRoutes);
+app.use("/headline-selections", headlineSelectionRoutes);
+app.use("/news", newsRoutes);
+app.use("/chats", userChatRoutes);
+app.use("/users", userRoutes);
 
 // Home route
-app.get('/', (req, res) => {
-    const topics = ['WORLD', 'NATIONAL', 'BUSINESS', 'TECHNOLOGY', 'ENTERTAINMENT', 'SPORTS', 'SCIENCE', 'HEALTH'];
-    res.render('home', { topics });
+app.get("/", (req, res) => {
+  const topics = [
+    "WORLD",
+    "NATIONAL",
+    "BUSINESS",
+    "TECHNOLOGY",
+    "ENTERTAINMENT",
+    "SPORTS",
+    "SCIENCE",
+    "HEALTH",
+  ];
+  res.render("home", { topics });
 });
 
 // // Route for fetching headlines based on topic
@@ -71,45 +82,45 @@ app.get('/', (req, res) => {
 // });
 // Route for fetching top headlines
 // Home route
-app.get('/', async (req, res) => {
-    const options = {
-        method: 'GET',
-        url: 'https://real-time-news-data.p.rapidapi.com/top-headlines',
-        params: { country: 'US', lang: 'en' },
-        headers: {
-            'X-RapidAPI-Key': '7e027ea54emshf9404a1862b3e29p1ea3d4jsndd6053e82d3d',
-            'X-RapidAPI-Host': 'real-time-news-data.p.rapidapi.com'
-        }
-    };
+app.get("/", async (req, res) => {
+  const options = {
+    method: "GET",
+    url: "https://real-time-news-data.p.rapidapi.com/top-headlines",
+    params: { country: "US", lang: "en" },
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPIDAPI_KEY,
+      "X-RapidAPI-Host": "real-time-news-data.p.rapidapi.com",
+    },
+  };
 
-    try {
-        const response = await axios.request(options);
-        res.render('home', { headlines: response.data.articles });
-    } catch (error) {
-        console.error('Error fetching top headlines:', error);
-        res.status(500).render('error', { error: 'Error fetching top headlines' });
-    }
+  try {
+    const response = await axios.request(options);
+    res.render("home", { headlines: response.data.articles });
+  } catch (error) {
+    console.error("Error fetching top headlines:", error);
+    res.status(500).render("error", { error: "Error fetching top headlines" });
+  }
 });
 
-
-const configureAssociations = require('./associations');
+const configureAssociations = require("./associations");
 // Configure model associations
 configureAssociations();
 
 // Synchronize Sequelize models with the database, then start the server
-sequelize.sync({ force: false })
-    .then(() => {
-        console.log('Database tables created');
-        app.listen(PORT, () => {
-            console.log(`Server running on http://127.0.0.1:${PORT}`);
-        });
-    })
-    .catch(error => {
-        console.error('Failed to synchronize database:', error);
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log("Database tables created");
+    app.listen(PORT, () => {
+      console.log(`Server running on http://127.0.0.1:${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.error("Failed to synchronize database:", error);
+  });
 
 // Error Handling Middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
